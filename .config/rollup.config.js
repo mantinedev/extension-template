8234bc9cd6ca66be769de036764cb4b9a6ad1eb3
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-22 12:47:35 +0300

    Setup build with rollup

---
+++ .config/rollup.config.js
@@ -0,0 +1,36 @@
+import path from "node:path";
+import fs from "node:fs";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeExternals from "rollup-plugin-node-externals";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import esbuild from "rollup-plugin-esbuild";
+import json from "@rollup/plugin-json";
+import replace from "@rollup/plugin-replace";
+
+const { name } = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../package.json"), "utf-8")
+);
+
+export default {
+  input: path.join(__dirname, "../src/index.ts"),
+  output: {
+    name,
+    format: "esm",
+    externalLiveBindings: false,
+    preserveModules: true,
+    dir: path.join(__dirname, "../dist"),
+    sourcemap: "inline",
+  },
+  plugins: [
+    commonjs(),
+    nodeExternals(),
+    nodeResolve({ extensions: [".ts", ".tsx", ".js", ".jsx"] }),
+    esbuild({
+      minify: false,
+      sourceMap: false,
+      tsconfig: path.resolve(process.cwd(), "tsconfig.json"),
+    }),
+    json(),
+    replace({ preventAssignment: true }),
+  ],
+};
