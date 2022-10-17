16c816e5cefb9ebd24e0142f049334b46578f228
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-17 19:58:48 +0400

    Add more items to page header

--- docs/components/PageHeader/PageHeader.styles.ts
+++ docs/components/PageHeader/PageHeader.styles.ts
@@ -3,7 +3,15 @@ import { createStyles } from '@mantine/core';
 export default createStyles((theme) => ({
   root: {
     paddingTop: 80,
-    paddingBottom: theme.spacing.xl,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    paddingBottom: 50,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+  },
+
+  title: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
+
+  description: {
+    maxWidth: 500,
   },
 }));
--- docs/settings.ts
+++ docs/settings.ts
@@ -1,5 +1,11 @@
 export const SETTINGS = {
+  packageName: 'mantine-extension-template',
+  packageDescription:
+    'Mantine extension starter kit: full development setup, premade build/publishing scripts, Next.js documentation template',
   repositoryUrl: 'https://github.com/mantinedev/mantine',
   npmUrl: 'https://www.npmjs.com/package/mantine-extension-template',
+  licenseLink: 'https://github.com/mantinedev/mantine/blob/master/LICENSE',
+  contributeLink: 'https://github.com/mantinedev/mantine/blob/master/CONTRIBUTING.md',
+  license: 'MIT',
   pageTitle: 'Mantine extension template',
 };
