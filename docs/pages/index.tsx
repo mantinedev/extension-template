fb8c183ffe93cb711c5f7fed9d1f22a67b91f9dc
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-17 13:15:51 +0400

    Init home page content

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,3 +1,12 @@
+import { Container } from '@mantine/core';
+import HomePageContent from '../mdx/home-page.mdx';
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <Container>
+        <HomePageContent />
+      </Container>
+    </div>
+  );
 }
