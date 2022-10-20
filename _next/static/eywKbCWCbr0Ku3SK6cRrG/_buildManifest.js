fd2086657f042e8c088144c26f3cf88d6c0810b2
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-20 19:46:37 +0400

    Updates

--- _next/static/eywKbCWCbr0Ku3SK6cRrG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/446-f5ec2fbcdac54e34.js","static/chunks/pages/index-18fa10388bd3b07a.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
