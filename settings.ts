78c1a748faab52ba76972740cb7d4fe9c30330a5
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-20 19:00:17 +0400

    Prepare for deployment to gh pages

--- settings.ts
+++ settings.ts
@@ -1,7 +1,7 @@
 export default {
   // Your username on GitHub
   // It is used to configure GitHub releases
-  githubUsername: 'rtivital',
+  githubUsername: 'mantinedev',
 
   // Your extension repository name, for example mantine-tag-picker
   // it is used to configure GitHub releases
