11512fcc640a989ff8d2957e73b2838b0d9e596b
Vitaly Rtishchev <rtivital@gmail.com>
2023-10-30 14:41:46 +0400

    Add dynamic repository resolution for docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,8 +1,21 @@
+const fs = require('fs-extra');
+const signale = require('signale');
+
+let repository;
+
+try {
+  const packageJson = fs.readJsonSync('../package/package.json');
+  repository = packageJson.repository.url.split('/').at(-1).replace('.git', '');
+} catch {
+  signale.error('Failed to read repository field of package/package.json\n');
+  process.exit(1);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/mantine-extension-template' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? `/${repository}` : undefined,
   pageExtensions: ['ts', 'tsx', 'mdx'],
   eslint: {
     ignoreDuringBuilds: true,
