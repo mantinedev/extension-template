fb7a3878a2c3e556bfbdb317ef5e83cf1956186a
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-03 11:53:04 +0400

    Change docs build command

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -23,6 +23,7 @@ try {
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? `/${repository}` : undefined,
   pageExtensions: ['ts', 'tsx', 'mdx'],
   eslint: {
