94bd9723364eaaf2492c256e6bac5180f35fa8c6
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-21 10:08:05 +0400

    Add missing header package styles imports

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 import '@mantine/code-highlight/styles.css';
 import '@mantinex/mantine-logo/styles.css';
+import '@mantinex/mantine-header/styles.css';
 import '@mantinex/demo/styles.css';
 import 'mantine-extension-template/styles.css';
 import React from 'react';
