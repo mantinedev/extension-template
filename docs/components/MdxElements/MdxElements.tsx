22272d1705c85c67633338a425b5e84558c50e1d
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-02-06 15:30:19 +0100

    🚨 fixes lint

--- docs/components/MdxElements/MdxElements.tsx
+++ docs/components/MdxElements/MdxElements.tsx
@@ -1,11 +1,11 @@
-import { Anchor, Code, Image, Title } from "@mantine/core";
-import { Demo } from "@mantinex/demo";
-import { CodeHighlight } from "@mantinex/shiki";
-import cx from "clsx";
-import type { MDXComponents } from "mdx/types";
-import React from "react";
-import { MdxInfo } from "../MdxInfo/MdxInfo";
-import classes from "./MdxElements.module.css";
+import React from 'react';
+import cx from 'clsx';
+import type { MDXComponents } from 'mdx/types';
+import { Anchor, Code, Image, Title } from '@mantine/core';
+import { Demo } from '@mantinex/demo';
+import { CodeHighlight } from '@mantinex/shiki';
+import { MdxInfo } from '../MdxInfo/MdxInfo';
+import classes from './MdxElements.module.css';
 
 export function MdxTitle({
   id,
