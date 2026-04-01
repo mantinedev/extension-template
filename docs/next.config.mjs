22c66b71a5cac0c6397d4ff4d4d895cd4666b5c4
Vitaly Rtishchev <rtivital@gmail.com>
2026-04-01 22:11:44 +0300

    Update Next and React dependencies

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,11 +1,10 @@
 import createMDX from '@next/mdx';
 import fs from 'fs-extra';
-import remarkSlug from 'remark-slug';
 import signale from 'signale';
 
 const withMDX = createMDX({
   options: {
-    remarkPlugins: [remarkSlug],
+    remarkPlugins: ['remark-slug'],
   },
 });
 
@@ -25,9 +24,6 @@ const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? `/${repository}` : undefined,
   pageExtensions: ['ts', 'tsx', 'mdx'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default withMDX(nextConfig);
